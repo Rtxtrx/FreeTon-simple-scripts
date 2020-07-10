9320c519ffb8cb8145e0a81f74e28ce98d5cbd3d
Sergey Tyurin <svt@zmail.ru>
2020-07-10 16:15:23 +0300

    improve copy and start new engine

--- Update_Node.sh
+++ Update_Node.sh
@@ -44,16 +44,19 @@ NEW_NODE_DIR=$HOME/upd.node
 BKP_OLD_NODE_DIR=$HOME/old.net.ton.dev
 # ===============================
 sudo apt -y install jq
+
 # Fetch & build new node
 cd $HOME
 rm -rf $NEW_NODE_DIR
 git clone https://github.com/tonlabs/net.ton.dev $NEW_NODE_DIR
 cd $NEW_NODE_DIR
-git checkout $TONLABS_REV || echo "### - ERROR: Wrong commit number." || exit 1
+git checkout master
+git fetch --all --prune
+git reset --hard origin/master
+
 cd $NEW_NODE_DIR/scripts
-
-"$NEW_NODE_DIR/scripts/build.sh" || echo "### - ERROR: Build failed." || exit 1
-
+source ./env.sh
+./build.sh
 # ===============================
 # Check build and commit nums
 cd $NEW_NODE_DIR
@@ -67,23 +70,13 @@ CURR_TON_REV=`git rev-parse HEAD`
 [[ ! -x $NEW_NODE_DIR/ton/build/validator-engine/validator-engine ]]  && echo "Can't find builded 'validator-engine'. New node doesnot build?" && exit 1
 # ===============================
 
-cd $HOME
-mkdir -p $HOME/bin
-cp -f $NEW_NODE_DIR/ton/build/lite-client/lite-client $HOME/bin
-cp -f $NEW_NODE_DIR/ton/build/utils/tonos-cli $HOME/bin
-cp -f $NEW_NODE_DIR/ton/build/validator-engine/validator-engine $HOME/bin
-cp -f $NEW_NODE_DIR/ton/build/validator-engine-console/validator-engine-console $HOME/bin
-
-cp -f $HOME/net.ton.dev/scripts/tonlabs-cli.conf.json $NEW_NODE_DIR/scripts/
-
-
 VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
 echo "Engine PID: $VAL_PID"
 
 read -p "### LAST CHANCE! Ready to UPGRADE AND RESTART NODE?  (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
-        echo "Killing..."
+        echo "Upgade..."
     ;;
     * )
         echo "Cancelled."
@@ -91,45 +84,63 @@ case ${answer:0:1} in
     ;;
 esac
 
-kill $VAL_PID
+# ===========================================
+# Copy new binaries to $HOME/bin
+cd $HOME
+mkdir -p $HOME/bin
+mkdir -p $HOME/logs
+cp -f $NEW_NODE_DIR/ton/build/lite-client/lite-client $HOME/bin
+cp -f $NEW_NODE_DIR/ton/build/utils/tonos-cli $HOME/bin
+cp -f $NEW_NODE_DIR/ton/build/validator-engine/validator-engine $HOME/bin
+cp -f $NEW_NODE_DIR/ton/build/validator-engine-console/validator-engine-console $HOME/bin
 
+# Copy tonlabs-cli.conf.json from current to new script dir
+cp -f  $WRK_NODE_DIR/scripts/tonlabs-cli.conf.json $NEW_NODE_DIR/scripts/
+
+# Copy my own scripts to new script dir
+cp -ur $WRK_NODE_DIR/scripts/* $NEW_NODE_DIR/scripts
+# ===========================================
+# Kill engine process
+echo "Killing engine..."
+kill $VAL_PID
 while true
 do
     VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
     #echo "### - Node steel works! Engine PID: $VAL_PID"
     if [[ -z $VAL_PID ]]; then
         echo "### Dead!"
-	break
+    break
     fi
     printf "."
 done
 
 # ===========================================
+# Backup old and move new to work
+echo "Moving..."
 mv -f $WRK_NODE_DIR $BKP_OLD_NODE_DIR
 mv -f $NEW_NODE_DIR $WRK_NODE_DIR
 echo "INFO: Moved!"
 # ===========================================
 
-mkdir -p $HOME/bin
-mkdir -p $HOME/logs
-cp -f $WRK_NODE_DIR/ton/build/lite-client/lite-client $HOME/bin
-cp -f $WRK_NODE_DIR/ton/build/utils/tonos-cli $HOME/bin
-cp -f $WRK_NODE_DIR/ton/build/validator-engine/validator-engine $HOME/bin
-cp -f $WRK_NODE_DIR/ton/build/validator-engine-console/validator-engine-console $HOME/bin
-cp -ur $BKP_OLD_NODE_DIR/scripts/* $WRK_NODE_DIR/scripts
-
 SCRIPT_DIR=$WRK_NODE_DIR/scripts
 cd $SCRIPT_DIR
-source "${SCRIPT_DIR}/env.sh"
 
 mv -f ${TON_WORK_DIR}/node.log $HOME/logs/
 
-echo "INFO: start NEW node..."
-
-"$HOME/bin/validator-engine" -C "${TON_WORK_DIR}/etc/ton-global.config.json" --db "${TON_WORK_DIR}/db" > "${TON_WORK_DIR}/node.log" 2>&1 &
+# echo "INFO: start NEW node..."
+# "$HOME/bin/validator-engine" -C "${TON_WORK_DIR}/etc/ton-global.config.json" --db "${TON_WORK_DIR}/db" > "${TON_WORK_DIR}/node.log" 2>&1 &
+./run.sh
 sleep 2s
 
 VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
+if [[ -z $VAL_PID ]]; then
+  while true
+  do
+    ./run.sh
+    sleep 2s
+    VAL_PID=`ps -ax | grep "validator\-engine" | awk '{print $1}'`
+    [[ ! -z $VAL_PID ]] && break
+    echo "### - ALARM !!! Can't start engine."
+  done
+fi
 echo "Engine PID: $VAL_PID"
-
-echo "INFO: log file = ${TON_WORK_DIR}/node.log"
